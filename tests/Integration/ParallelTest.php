50e664352d2e2be5f0dd64433b57d93d294e747f
Alexander Cheprasov <alexander@cheprasov.com>
2016-02-04 00:28:00 +0300

    Added Tests for nested Parallel objects

--- tests/Integration/ParallelTest.php
+++ tests/Integration/ParallelTest.php
@@ -38,7 +38,7 @@ class ParallelTest extends \PHPUnit_Framework_TestCase {
     }
 
     public function test_viaMemcachedStorage() {
-        $Parallel = new Parallel(new MemcachedStorage(['servers'=>[['127.0.0.1', 11211]]]));
+        $Parallel = new Parallel(new MemcachedStorage(['servers'=>[explode(':', TEST_MEMCACHED_SERVER)]]));
 
         $time = microtime(true);
         for ($i = 1; $i <= 5; ++$i) {
@@ -54,6 +54,66 @@ class ParallelTest extends \PHPUnit_Framework_TestCase {
         $this->assertLessThan(6, $time);
 
         $this->assertSame(['n:1' => 1, 'n:2' => 4, 'n:3' => 9, 'n:4' => 16, 'n:5' => 25], $result);
+    }
+
+    public function test_nestedViaApcuStorage() {
+        $Parallel = new Parallel(new ApcuStorage());
+
+        $time = microtime(true);
+        for ($i = 1; $i <= 2; ++$i) {
+            $Parallel->run('n:'. $i, function() use ($i) {
+                $Parallel = new Parallel(new ApcuStorage());
+                $Parallel->run('n:1', function() {
+                    sleep(2);
+                    return 'foo';
+                });
+                $Parallel->run('n:2', function() {
+                    sleep(2);
+                    return 'bar';
+                });
+                return $Parallel->wait(['n:1', 'n:2']);
+            });
+        }
+        sleep(2);
+        $result = $Parallel->wait(['n:1', 'n:2']);
+        $time = microtime(true) - $time;
+        $this->assertGreaterThanOrEqual(2, $time);
+        $this->assertLessThan(3, $time);
+
+        $this->assertSame(
+            ['n:1' => ['n:1' => 'foo', 'n:2' => 'bar'], 'n:2' => ['n:1' => 'foo', 'n:2' => 'bar']],
+            $result
+        );
+    }
+
+    public function test_nestedViaMemcachedStorage() {
+        $Parallel = new Parallel(new MemcachedStorage(['servers'=>[explode(':', TEST_MEMCACHED_SERVER)]]));
+
+        $time = microtime(true);
+        for ($i = 1; $i <= 2; ++$i) {
+            $Parallel->run('n:'. $i, function() use ($i) {
+                $Parallel = new Parallel(new MemcachedStorage(['servers'=>[explode(':', TEST_MEMCACHED_SERVER)]]));
+                $Parallel->run('n:1', function() {
+                    sleep(2);
+                    return 'foo';
+                });
+                $Parallel->run('n:2', function() {
+                    sleep(2);
+                    return 'bar';
+                });
+                return $Parallel->wait(['n:1', 'n:2']);
+            });
+        }
+        sleep(2);
+        $result = $Parallel->wait(['n:1', 'n:2']);
+        $time = microtime(true) - $time;
+        $this->assertGreaterThanOrEqual(2, $time);
+        $this->assertLessThan(3, $time);
+
+        $this->assertSame(
+            ['n:1' => ['n:1' => 'foo', 'n:2' => 'bar'], 'n:2' => ['n:1' => 'foo', 'n:2' => 'bar']],
+            $result
+        );
     }
 
 }
